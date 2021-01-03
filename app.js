41c78d07f560ce0cc8a58a4ffa17b493c3cb3749
asiimwedismas <asiimwedismas@gmail.com>
2021-01-04 00:49:38 +0300

    add: config files

--- app.js
+++ app.js
@@ -2,15 +2,16 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const config = require('config');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
 
-app.use(logger('dev'));
+if (app.get('env') === 'development') app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
