b95a02782b5caa8954ef24b4ec5f1498cba23add
asiimwedismas <asiimwedismas@gmail.com>
2021-01-07 18:37:45 +0300

    update: add customers, genres, movies and rentals routers

--- app.js
+++ app.js
@@ -3,8 +3,17 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const config = require('config');
+const mongoose = require('mongoose');
 
-const indexRouter = require('./routes/index');
+const db = config.get('db');
+mongoose.
+    connect(db, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false}).
+    catch(err => console.log(`Error connecting to ${db}... ${err}`));
+
+const customersRouter = require('./routes/customers');
+const genresRouter = require('./routes/genres');
+const moviesRouter = require('./routes/movies');
+const rentalsRouter = require('./routes/rentals');
 const usersRouter = require('./routes/users');
 
 const app = express();
@@ -15,7 +24,10 @@ app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/customers', customersRouter);
+app.use('/api/genres', genresRouter);
+app.use('/api/movies', moviesRouter);
+app.use('/api/rentals', rentalsRouter);
+app.use('/api/users', usersRouter);
 
 module.exports = app;
