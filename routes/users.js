0f59b9c00e2216a5b64581605d41b47196d6258c
asiimwedismas <asiimwedismas@gmail.com>
2021-05-27 20:48:49 +0300

    code refactor:

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,27 @@
+const bcrypt = require('bcrypt');
+const _ = require('lodash');
+const {User, validateUser} = require('../models/user');
+const auth = require('../middleware/auth');
+const validateReqBody = require('../middleware/validateReqBody');
 const express = require('express');
 const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/me', auth, async (req, res) => {
+  const user = await User.findById(req.user._id).select('-password');
+  res.send(user);
+});
+
+router.post('/', validateReqBody(validateUser), async (req, res) => {
+  let user = await User.findOne({email: req.body.email});
+  if (user) return res.status(400).send('User already registered.');
+
+  user = new User(_.pick(req.body, ['name', 'email', 'password']));
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
+  await user.save();
+
+  const token = user.generateAuthToken();
+  res.header('x-auth-token', token).send(_.pick(user, ['_id', 'name', 'email']));
 });
 
 module.exports = router;
